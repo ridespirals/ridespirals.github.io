e3bfcadea6acff3437859c4d955d65306f884bb0
John Varga <john.g.varga@gmail.com>
2017-02-22 17:33:22 -0800

    log for testing

--- modules/App.js
+++ modules/App.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 export default React.createClass({
+    componentDidMount() {
+        console.log(`Hello from App module! I mounted!`)
+    },
     render() {
         return (
             <div>hi from React. get over it!</div>
